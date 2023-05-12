0206f10801865aee44438725ab3e16859d5b2df5
marshallhch <marshallhch@gmail.com>
2023-05-12 12:48:31 +0900

    signup 진행중 0502

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   // print_r($path_name);
   // print_r($path_name[3]);
 
-  if($path_name[3] == 'register' && $path_name[4] == 'signup'){
+  if($path_name[3] == 'register' && $path_name[4] == 'signup' && $_SERVER['REQUEST_METHOD'] == "POST"){
     include $_SERVER['DOCUMENT_ROOT'].'/main_project_vom13/baexang_back/register/signup.php';
   } else {
     echo 'index.php page!!';
