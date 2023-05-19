8c17fc75bcf5243028d3c32fa46f5a2c1105bac4
marshallhch <marshallhch@gmail.com>
2023-05-19 12:51:03 +0900

    회원관리 접근제한 코드 구현 중 0519

---
+++ admin/admin.php
@@ -0,0 +1,66 @@
+<?php
+
+  class Admin{
+    private $conn;
+    private $table = 'bx_user';
+
+    public $lvl;
+    public $token;
+
+    public function __construct($db){
+      $this->conn = $db;
+    }
+
+    public function get_users(){
+      session_start();
+      $lvl = 0;
+      $token = '';
+
+      if(!isset($_SESSION['usertoken'])){
+        $token = '';
+      } else {
+        $token = $_SESSION['usertoken'];
+      }
+
+      if($_SESSION['userlvl'] != 1){
+        $lvl = 0;
+      } else {
+        $lvl = 1;
+      }
+
+      if($token == '' || $lvl == 0){
+        return false;
+      } else {
+        $sql = "SELECT * FROM ". $this->table ." WHERE user_lvl=:lvl AND user_token=:token";
+        $stmt = $this->conn->prepare($sql);
+
+        $this->lvl = htmlspecialchars(strip_tags($lvl));
+        $this->token = htmlspecialchars(strip_tags($token));
+
+        // echo $this->token;
+        // echo '------';
+        // echo $this->lvl;
+        // exit;
+
+        $stmt->bindParam(":lvl", $this->lvl);
+        $stmt->bindParam(":token", $this->token);
+        $stmt->execute();
+
+        print_r($stmt->execute());
+        // $num = $stmt->rowCount();
+
+        // if($num > 0){
+        //   echo '권한 있음';
+        // } else {
+        //   echo '권한 없음';
+        // }
+      }
+      // $sql = "SELECT * FROM ". $this->table ." WHERE user_lvl != 1 ORDER BY user_idx DESC";  
+      // $stmt = $this->conn->prepare($sql);
+      // $stmt->execute();
+      
+      // return $stmt;
+    }
+  }
+
+?>
